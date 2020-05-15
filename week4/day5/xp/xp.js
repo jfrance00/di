c5a30276e9879e3999fc2bc8e0ae1e482b118c74
jfrance00 <jfrance00@gmail.com>
2020-05-15 17:15:55 +0300

    updated mini project: no design but no bugs

--- week4/day5/xp/xp.js
+++ week4/day5/xp/xp.js
@@ -1,7 +1,4 @@
-console.log("it is connected");
-
-
-let operators = ["+", "-", "x", "/"]
+let operators = ["+", "-", "*", "/"]
 let numbers = ['0','1','2','3','4','5','6','7','8','9','.'] //should be able to get #s from DOM
 let num = [];
 
@@ -14,49 +11,28 @@ function my_f(input){
     return num;
   } else if (operators.includes(input)){
     console.log("operator");
-    convert_int(num);        //user done input of first number, converts array to int
-    clear_input();              // clears array num for future inputs
-    action = input;
-    return action;
+    num.push(input);
+    return num;
   } else if (input == "="){
     calculate();
   }
 }
 
-function convert_int(num){
-  num1 = Number(num.join(""));
-  return num1;
-}
 
 function calculate(){
-  num2 = Number(num.join(""));  //should find a way to make variable names dynamic so can have one function for this
-  if (action == "+"){           // if addition
-    result = num1 + num2;
-    return result
-  } else if (action == "-") {     //subtraction
-    result = num1 - num2;
-    return result;
-  } else if (action == "x"){    //multiplication
-    result = num1 * num2;
-    return result;
-  } else if (action == "/"){  //division
-    result = num1 / num2;
-    return result;
-  }
+  solution = eval(num.join(""));
+  console.log(solution);
+  reset();
 }
 
 function reset(){
-  num1 = 0
-  num2 = 0
+  length = num.length;
+  num.splice(0,length);
+  return num;
 }
 
 function clear(){   //removes last number
   num.pop();
   console.log(num);
-  return num;
-}
-
-function clear_input(){
-  num = [];
   return num;
 }
