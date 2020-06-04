2c2dd14fe90c30b7b5b092d94ee7ff1d5c9d7f39
jfrance00 <jfrance00@gmail.com>
2020-06-04 13:25:33 +0300

    Fixed spacing

--- week7/day3/daily.py
+++ week7/day3/daily.py
@@ -18,37 +18,57 @@ def longest_list(matrix):
             longest = item
     return len(longest)
 
-def check_letters(char):
-    valid_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    part_of_message = False
-    if char in valid_chars:
-        part_of_message = True
-    return part_of_message
-
-def record_letter(char):
-    global hidden_message
-    hidden_message = hidden_message + char
-    print(hidden_message)
-    return hidden_message
 
 def square_lists(list):
     list.append(' ')
     return list
 
 
+def is_value_letter(char):
+    valid_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    part_of_message = False
+    if char in valid_chars:
+        part_of_message = True
+    return part_of_message
+
+
+def is_value_other(char):
+    is_space = False
+    valid_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 "
+    if (char not in valid_chars):
+        is_space = True
+    return is_space
+
+
+def record_letter(char):
+    global hidden_message
+    print("adding {}".format(char))
+    hidden_message = hidden_message + char
+    return hidden_message
+
+
+def record_space(char):
+    global hidden_message
+    print("adding space")
+    space = " "
+    hidden_message = hidden_message + space
+    return hidden_message
+
+
+
 def scan_matrix(matrix):
     loop_length = longest_list(matrix)
     loop = 0
     while loop < loop_length:
-        print(" ")
         for index, item in enumerate(matrix):
             if (len(item) < 3):
                 square_lists(item)
-            print("index: {}, loop: {}".format(index, loop))
-            value  = item[loop]   #gets an error that index out of range
-            print(value)
-            if (check_letters(value)):
+            value = item[loop]
+            print("value being checked is: ", value)
+            if (is_value_letter(value)):
                 message = record_letter(value)
+            elif (is_value_other(value)):
+                message = record_space(value)
         loop += 1
     print(message)
 
