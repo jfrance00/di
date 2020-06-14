18e85fcfc2c005c2729205478ac2cb9e1e2a490c
jfrance00 <jfrance00@gmail.com>
2020-06-14 13:11:34 +0300

    finished daily challenge

--- week8/day3/daily.py
+++ week8/day3/daily.py
@@ -1,22 +1,15 @@
 import random
 
-
 class DNA():
 
     def __init__(self):
         self.dna = []
-
-    def make_genome(self):
         index = 0
         while (index < 10):
             chromosome = Chromosome()
             chromosome = chromosome.define_chromosome()
             self.dna.append(chromosome)
             index += 1
-        print(len(self.dna))
-
-    def display_genome(self):
-        print(self.dna)
 
 
 class Chromosome():
@@ -33,9 +26,66 @@ class Chromosome():
 
 
 
-organism = DNA()
-my_chromosome = Chromosome()
-organism.make_genome()
+class organisme():
 
-# organism.make_genome()
-# print(organism.make_genome())
+    def __init__(self, dna, organism_type, environment):      #where environment is the % change of a gene changing
+        self.dna = dna
+        self.environment = environment
+        self.organism_type = organism_type
+
+    def mutate(self):
+        index = 0
+        while (index < self.environment):
+            chromosome_index = random.randint(0,9)               # pick random chromosome
+            random_chromosome = self.dna[chromosome_index]
+            gene_index = random.randint(0,9)                     # pick random gene
+            if random_chromosome[gene_index] == 0:
+                random_chromosome[gene_index] = 1
+            # else:                                              # if mutation truly random and genes can switch in both
+            #     random_chromosome[gene_index] = 0              # directions than it almost always takes over a million generations
+            random_chromosome = self.dna[chromosome_index]
+            index += 1
+        return self.dna
+
+    def check_dna(self):
+        for chromosome in self.dna:
+            if 0 in chromosome:
+                return False
+        return True
+
+
+    def evolve(self):
+        generation = 0
+        while generation < 500:
+            if self.check_dna() == False:
+                self.mutate()
+                generation += 1
+            else:
+                print(f' {self.organism_type} evolved to have a genome of all ones in {generation} generations')
+                return generation
+        print(f'{self.organism_type} took over a million generations to get a sequence of all ones')
+        print(f'{self.organism_type} took over a million generations to get a sequence of all ones')
+
+
+
+
+def main():
+    organism1 = DNA()                 # TODO find a cleaner, less repetitive way to do this
+    organism2 = DNA()
+    organism3 = DNA()
+    organism1 = organisme(organism1.dna, "neanderthal", 4)
+    organism2 = organisme(organism2.dna, "ape", 2)
+    organism3 = organisme(organism3.dna, "homosapien", 10)
+
+    organism1.evolve()
+    organism2.evolve()
+    organism3.evolve()
+
+
+main()
+
+#Create a new class called `Organismè that accepts a DNA object and
+# a environement parameter that sets the probability for its DNA to mutate.
+
+#make several organisms that accept DNA object and environment parameter
+#specifying likelihood of mutation. Track how long it takes to mutate to all ones
